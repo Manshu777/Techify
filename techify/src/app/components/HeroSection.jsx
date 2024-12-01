02f5a0e1b40f371097fee71b5ebc2bde01d225be
Disha <131692855+Disha-khurana@users.noreply.github.com>
2024-12-01 15:44:34 +0530

    responsive hero sec

--- techify/src/app/components/HeroSection.jsx
+++ techify/src/app/components/HeroSection.jsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section id="hero" className=" bg-[#f0f4ff] ">
+    <section id="hero" className=" bg-[#f0f4ff]">
       {/* Hero Content */}
-      <div className="container lg:flex items-center justify-center lg:justify-between gap-10 mx-auto px-5 md:px-10 lg:px-32 py-10 relative z-10">
+      <div className=" lg:flex items-center justify-center lg:justify-between gap-10 mx-auto px-5 md:px-14 xl:px-32 py-10  relative z-10">
         <div className="pb-10 block lg:hidden">
           <Image
             src="/heroimg.webp"
@@ -18,10 +18,10 @@ const HeroSection = () => {
           />
         </div>
         <div className="w-full lg:w-[70%] space-y-5 md:space-y-6 lg:space-y-7">
-          <span className="text-lg lg:text-xl font-medium text-[#684df4]">
+          <span className="text-base lg:text-xl font-medium text-[#684df4]">
             BEST MARKETING SERVICE
           </span>
-          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-gray-800 leading-tight">
+          <h1 className="text-4xl md:text-[57px] xl:text-7xl font-bold text-gray-800 leading-tight">
             Make The Easiest <br className="hidden lg:block" /> Solution For You
           </h1>
           <p className="text-sm md:text-base text-gray-600 leading-relaxed">
@@ -31,22 +31,22 @@ const HeroSection = () => {
           </p>
 
           {/* Buttons */}
-          <div className="flex  items-center gap-8 lg:justify-start">
+          <div className="flex flex-wrap items-center gap-6 lg:justify-start">
             <Link
               href="/about-us"
-              className="bg-[#684df4] hover:bg-black click-btn btn-style text-white font-medium px-6 py-6 rounded-md"
+              className="bg-[#684df4] hover:bg-black click-btn btn-style text-white font-medium px-4 md:px-6 py-4 md:py-6 rounded-md"
             >
               ABOUT US
               <i className="fas fa-long-arrow-right ml-2"></i>
             </Link>
             <div className="flex items-center gap-3">
-              <div class="wrapper">
-                <div class="video-main">
-                  <div class="promo-video">
-                    <div class="waves-block">
-                      <div class="waves wave-1"></div>
-                      <div class="waves wave-2"></div>
-                      <div class="waves wave-3"></div>
+              <div className="wrapper">
+                <div className="video-main">
+                  <div className="promo-video">
+                    <div className="waves-block">
+                      <div className="waves wave-1"></div>
+                      <div className="waves wave-2"></div>
+                      <div className="waves wave-3"></div>
                     </div>
                   </div>
                   <a
@@ -61,11 +61,11 @@ const HeroSection = () => {
               <div>
                 <a
                   href="https://www.youtube.com/watch?v=_sI_Ps7JSEk"
-                  className="block text-base font-medium text-black hover:underline"
+                  className="block text-sm md:text-base font-medium text-black hover:underline"
                 >
                   Watch Our Story
                 </a>
-                <span className="block text-base text-gray-500 font-normal">
+                <span className="block text-sm md:text-base text-gray-500 font-normal">
                   Subscribe Now
                 </span>
               </div>
