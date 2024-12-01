1ac63d948ad5bfe8cc690d1bb746879c49ffa502
Disha <131692855+Disha-khurana@users.noreply.github.com>
2024-12-01 14:30:50 +0530

    updated hero section

--- techify/src/app/components/HeroSection.jsx
+++ techify/src/app/components/HeroSection.jsx
@@ -1,68 +1,87 @@
-'use client';
+"use client";
 import { FaPlay } from "react-icons/fa";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <section id="hero" className=" bg-[#f0f4ff] ">
-    
-
       {/* Hero Content */}
-      <div className="container flex items-center justify-between gap-10 mx-auto px-5 md:px-10 lg:px-32 py-10 relative z-10">
-       
-          <div className="w-[70%] space-y-7">
-            <span className="text-lg md:text-xl font-medium text-[#684df4]">BEST MARKETING SERVICE</span>
-            <h1 className="text-3xl md:text-7xl font-bold text-gray-800 leading-tight">
-              Make The Easiest <br /> Solution For You
-            </h1>
-            <p className="text-sm md:text-base text-gray-600 leading-relaxed">
-              Energistically harness ubiquitous imperatives without state of the art collaboration and idea-sharing.
-              Monotonectally parallel task cross-unit experiences and front-end.
-            </p>
+      <div className="container lg:flex items-center justify-center lg:justify-between gap-10 mx-auto px-5 md:px-10 lg:px-32 py-10 relative z-10">
+        <div className="pb-10 block lg:hidden">
+          <Image
+            src="/heroimg.webp"
+            alt="Shape 3"
+            width={500}
+            height={500}
+            className=" w-full h-full"
+          />
+        </div>
+        <div className="w-full lg:w-[70%] space-y-5 md:space-y-6 lg:space-y-7">
+          <span className="text-lg lg:text-xl font-medium text-[#684df4]">
+            BEST MARKETING SERVICE
+          </span>
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-gray-800 leading-tight">
+            Make The Easiest <br className="hidden lg:block" /> Solution For You
+          </h1>
+          <p className="text-sm md:text-base text-gray-600 leading-relaxed">
+            Energistically harness ubiquitous imperatives without state of the
+            art collaboration and idea-sharing. Monotonectally parallel task
+            cross-unit experiences and front-end.
+          </p>
 
-            {/* Buttons */}
-            <div className="flex flex-col sm:flex-row items-center gap-8 justify-center lg:justify-start">
-              <Link
-                href="/about-us"
-                className="bg-[#684df4] hover:bg-black click-btn btn-style text-white font-medium px-6 py-6 rounded-md"
-              >
-                ABOUT US
-                <i className="fas fa-long-arrow-right ml-2"></i>
-              </Link>
-              <div className="flex items-center gap-3">
-                <a
-                  href="https://www.youtube.com/watch?v=_sI_Ps7JSEk"
-                  className="w-12 h-12 flex items-center justify-center bg-[#684df4]  rounded-full text-white text-lg transition duration-300"
-                >
-                  <FaPlay />
-                </a>
-                <div>
+          {/* Buttons */}
+          <div className="flex  items-center gap-8 lg:justify-start">
+            <Link
+              href="/about-us"
+              className="bg-[#684df4] hover:bg-black click-btn btn-style text-white font-medium px-6 py-6 rounded-md"
+            >
+              ABOUT US
+              <i className="fas fa-long-arrow-right ml-2"></i>
+            </Link>
+            <div className="flex items-center gap-3">
+              <div class="wrapper">
+                <div class="video-main">
+                  <div class="promo-video">
+                    <div class="waves-block">
+                      <div class="waves wave-1"></div>
+                      <div class="waves wave-2"></div>
+                      <div class="waves wave-3"></div>
+                    </div>
+                  </div>
                   <a
                     href="https://www.youtube.com/watch?v=_sI_Ps7JSEk"
-                    className="block text-base font-medium text-black hover:underline"
+                    className="w-12 h-12 flex items-center justify-center bg-[#684df4]  rounded-full text-white text-lg transition duration-300"
                   >
-                    Watch Our Story
+                    <FaPlay />
                   </a>
-                  <span className="block text-base text-gray-500 font-normal">Subscribe Now</span>
                 </div>
+              </div>
+
+              <div>
+                <a
+                  href="https://www.youtube.com/watch?v=_sI_Ps7JSEk"
+                  className="block text-base font-medium text-black hover:underline"
+                >
+                  Watch Our Story
+                </a>
+                <span className="block text-base text-gray-500 font-normal">
+                  Subscribe Now
+                </span>
               </div>
             </div>
           </div>
-  
-      
+        </div>
 
-     
-   
-      <div className="">
-        <Image
-          src="/heroimg.webp"
-          alt="Shape 3"
-          width={500}
-          height={500}
-          className="w-full h-full"
-        />
-      </div>
+        <div className="">
+          <Image
+            src="/heroimg.webp"
+            alt="Shape 3"
+            width={500}
+            height={500}
+            className="hidden lg:block w-full h-full"
+          />
+        </div>
       </div>
     </section>
   );
--- techify/src/app/components/Navbar.jsx
+++ techify/src/app/components/Navbar.jsx
@@ -6,11 +6,17 @@ import { IoIosArrowDown } from 'react-icons/io';
 import { FaArrowRight } from 'react-icons/fa6';
 import { FiMenu } from 'react-icons/fi';
 import { MdKeyboardArrowRight } from 'react-icons/md';
-import { BsPlus } from 'react-icons/bs';
+import { BsDash, BsPlus } from 'react-icons/bs';
 import { RxCross2 } from 'react-icons/rx';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const [openMenu, setOpenMenu] = useState(null);
+
+  const toggleDropdown = (menu) => {
+    setOpenMenu(openMenu === menu ? null : menu);
+  };
 
   return (
     <div className="bg-[#e1eaff] px-5 md:px-14 xl:px-32 py-4 md:py-5 relative">
@@ -99,7 +105,7 @@ function Navbar() {
 
         {/* Three-Line Menu */}
         <div className="lg:hidden">
-          <button onClick={() => setMenuOpen(!menuOpen)} className="text-[#141d38] text-2xl">
+          <button onClick={() => setMenuOpen(!menuOpen)} className="text-white p-3 rounded-md bg-[#684df4] text-2xl">
             <FiMenu />
           </button>
         </div>
@@ -115,69 +121,109 @@ function Navbar() {
 
       {/* Sliding Menu */}
       <div
-        className={`fixed top-0 left-0 w-72 h-full bg-white border-r-[3px] border-[#684df4] shadow-lg transform ${
-          menuOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-500 ease-in-out z-20`}
-      >
-        <div className="flex items-center justify-between bg-[#e1eaff] py-5 px-5">
-          <Image
-            src="/logo.svg"
-            height={200}
-            width={200}
-            alt="logo"
-            className="h-auto w-auto max-h-12 md:max-h-16"
-          />
-        </div>
+      className={`fixed top-0 left-0 w-72 h-full bg-white border-r-[3px] border-[#684df4] shadow-lg transform ${
+        menuOpen ? 'translate-x-0' : '-translate-x-full'
+      } transition-transform duration-500 ease-in-out z-20`}
+    >
+      <div className="flex items-center justify-between bg-[#e1eaff] py-5 px-5">
+        <Image
+          src="/logo.svg"
+          height={200}
+          width={200}
+          alt="logo"
+          className="h-auto w-auto max-h-12 md:max-h-16"
+        />
+      </div>
 
-        {/* Close Button (Outside the Menu) */}
-        {menuOpen && (
-          <button
-            onClick={() => setMenuOpen(false)}
-            className="fixed top-5 left-[270px] text-xl bg-[#684df4] rounded-full p-2 text-white z-30"
-          >
-            <RxCross2 />
-          </button>
-        )}
+      {/* Close Button */}
+      {menuOpen && (
+        <button
+          onClick={() => setMenuOpen(false)}
+          className="fixed top-5 left-[270px] text-xl bg-[#684df4] rounded-full p-2 text-white z-30"
+        >
+          <RxCross2 />
+        </button>
+      )}
 
-        <ul className="mt-20 space-y-4 px-5 font-semibold text-[#141d38]">
-          <li className="flex items-center border-b border-pink-200 pb-3">
-            <MdKeyboardArrowRight className="text-xl" />
-            <Link href="/" onClick={() => setMenuOpen(false)}>
-              HOME
-            </Link>
-          </li>
-          <li className="flex items-center border-b border-pink-200 pb-3">
-            <MdKeyboardArrowRight className="text-xl" />
-            <Link href="/about" onClick={() => setMenuOpen(false)}>
-              ABOUT US
-            </Link>
-          </li>
-          <li className="flex items-center justify-between border-b border-pink-200 pb-3">
-            <div className="flex items-center">
+      <ul className="mt-20 space-y-4 px-5 font-semibold text-[#141d38]">
+        <li className="flex items-center border-b border-pink-200 pb-3">
+          <MdKeyboardArrowRight className="text-xl" />
+          <Link href="/" onClick={() => setMenuOpen(false)}>
+            HOME
+          </Link>
+        </li>
+        <li className="flex items-center border-b border-pink-200 pb-3">
+          <MdKeyboardArrowRight className="text-xl" />
+          <Link href="/about" onClick={() => setMenuOpen(false)}>
+            ABOUT US
+          </Link>
+        </li>
+        <li className="flex flex-col border-b border-pink-200 pb-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center ">
               <MdKeyboardArrowRight className="text-xl" />
               <Link href="/services" onClick={() => setMenuOpen(false)}>
                 SERVICES
               </Link>
             </div>
-            <BsPlus className="text-xl bg-gray-100 rounded-full " />
-          </li>
-          <li className="flex items-center justify-between border-b border-pink-200 pb-3">
+            <button
+              onClick={() => toggleDropdown('services')}
+              className="text-xl bg-gray-100 rounded-full"
+            >
+              {openMenu === 'services' ? <BsDash /> : <BsPlus />}
+            </button>
+          </div>
+          {openMenu === 'services' && (
+            <ul className="mt-2 space-y-2 pl-8 transition-transform ease-in-out duration-700">
+              <li className='border-y border-pink-200 py-3'>
+                <Link href="/services/web-development" >Web Development</Link>
+              </li>
+              <li className='border-b border-pink-200 pb-3'>
+                <Link href="/services/mobile-development">Mobile Development</Link>
+              </li>
+              <li >
+                <Link href="/services/seo">SEO</Link>
+              </li>
+            </ul>
+          )}
+        </li>
+        <li className="flex flex-col border-b border-pink-200 pb-3">
+          <div className="flex items-center justify-between">
             <div className="flex items-center">
               <MdKeyboardArrowRight className="text-xl" />
               <Link href="/blog" onClick={() => setMenuOpen(false)}>
                 BLOG
               </Link>
             </div>
-            <BsPlus className="text-xl bg-gray-100 rounded-full " />
-          </li>
-          <li className="flex items-center">
-            <MdKeyboardArrowRight className="text-xl" />
-            <Link href="/contact" onClick={() => setMenuOpen(false)}>
-              CONTACT US
-            </Link>
-          </li>
-        </ul>
-      </div>
+            <button
+              onClick={() => toggleDropdown('blog')}
+              className="text-xl bg-gray-100 rounded-full"
+            >
+              {openMenu === 'blog' ? <BsDash /> : <BsPlus />}
+            </button>
+          </div>
+          {openMenu === 'blog' && (
+            <ul className="mt-2 space-y-2 pl-8 transition-all duration-300">
+              <li className='border-y border-pink-200 py-3'>
+                <Link href="/blog/tech">Tech</Link>
+              </li>
+              <li className='border-b border-pink-200 pb-3'>
+                <Link href="/blog/lifestyle">Lifestyle</Link>
+              </li>
+              <li >
+                <Link href="/blog/travel">Travel</Link>
+              </li>
+            </ul>
+          )}
+        </li>
+        <li className="flex items-center">
+          <MdKeyboardArrowRight className="text-xl" />
+          <Link href="/contact" onClick={() => setMenuOpen(false)}>
+            CONTACT US
+          </Link>
+        </li>
+      </ul>
+    </div>
     </div>
   );
 }
